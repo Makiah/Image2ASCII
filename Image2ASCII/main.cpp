40df85b770d12a7a901024afe207dce7e6faefc1
Makiah <makiahtbennett@gmail.com>
2018-12-22 14:01:07 -0500

    Successfully pass and display image

--- Image2ASCII/main.cpp
+++ Image2ASCII/main.cpp
@@ -6,10 +6,27 @@
 //  Copyright © 2018 Makiah Bennett. All rights reserved.
 //
 
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <string>
+int main( int argc, char** argv )
+{
+    cv::String imageName( "../data/HappyFish.jpg" ); // by default
+    if( argc > 1)
+    {
+        imageName = argv[1];
+    }
+    cv::Mat image;
+    image = imread( imageName, cv::IMREAD_COLOR ); // Read the file
+    if( image.empty() )                      // Check for invalid input
+    {
+        std::cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+    namedWindow( "Display window", cv::WINDOW_AUTOSIZE ); // Create a window for display.
+    imshow( "Display window", image );                // Show our image inside it.
+    cv::waitKey(0); // Wait for a keystroke in the window
     return 0;
 }
