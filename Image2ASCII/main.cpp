689a9c0e203604da12d668fa89f0148c4218b0c4
Makiah <makiahtbennett@gmail.com>
2018-12-22 13:08:30 -0500

    Set up OpenCV3

---
+++ Image2ASCII/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Image2ASCII
+//
+//  Created by Makiah Bennett on 12/22/18.
+//  Copyright © 2018 Makiah Bennett. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
